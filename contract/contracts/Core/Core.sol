dddf0bc1e8f89ab9aaba713b23b7c7d5441cf1ee
shikhar360 <sssonishikhar360@gmail.com>
2023-01-25 20:02:03 +0530

    investor funtion added

--- contract/contracts/Core/Core.sol
+++ contract/contracts/Core/Core.sol
@@ -4,15 +4,19 @@ pragma solidity ^0.8.9;
 import "../HSBTFactory.sol";
 import "./StartupDetails.sol";
 
+
 error YOU_HAVENT_BOUGHT_TOKEN();
 error BE_STARTUP_OWNER();
 error ONLY_INVESTOR_FUNCTION();
 
 contract Core {
-   bytes32 internal constant STARTUP = keccak256("STARTUP");
-   bytes32 public constant INVESTOR = keccak256("INVESTOR");
+
+  
    HatcherySBT public sbtFactory;
    uint private s_ID = 0;
+   uint private i_ID = 0;
+   uint256[] private pushIDX ;
+
 
     constructor(address _address){
      sbtFactory = HatcherySBT(_address);
@@ -20,21 +24,25 @@ contract Core {
     }
     
    S_Details[] internal startupList;
+   
+
    mapping(address => uint256) internal atIndex;
    mapping(address => bool) internal isAlreadyStartupOwner;
    mapping(address => string) internal attachVideoHash;
+   mapping(address => uint256[]) internal investedToStartup;
+   mapping(address => uint256) internal totalInvested;
 
    /*
    *@dev Adding startups to List
    */
    
-   function addStartupsTOList ( string memory _name , string memory _descrip , uint256 _amt  ) external onlyStartupOwner onlyInvestors {
+   function addStartupsToList ( string memory _name , string memory _descrip , string memory _tagline, uint256 _amt  ) external onlyStartup onlyInvestors {
    
     if(isAlreadyStartupOwner[msg.sender]){
         startupList[atIndex[msg.sender]].isActive = false ;
     }
 
-    startupList.push(S_Details( s_ID , _name , _descrip , _amt , payable(msg.sender) , 0  , true ));
+    startupList.push(S_Details( s_ID , _name , _descrip , _tagline , _amt , payable(msg.sender) , 0  , true ));
 
     atIndex[msg.sender] = s_ID ;
     
@@ -47,32 +55,35 @@ contract Core {
    /*
    *@dev Adding video hash to startups
    */
-    function addVideoHash( string memory _hash) external onlyStartupOwner onlyInvestors {
+    function addVideoHash( string memory _hash) external onlyStartup onlyInvestors {
        if(isAlreadyStartupOwner[msg.sender]){
         attachVideoHash[msg.sender] = _hash;
        }else {
            revert BE_STARTUP_OWNER();
        }
     }
+
    
-    function getVideoHash() external view returns(string memory){
-        return attachVideoHash[msg.sender];
-    }
-
-
-
+    
    /*
    *@dev Adding Investments Function
    */
-   function amountOfTokenToBuy( address _startupOwner) external onlyInvestors payable {
+   function amountInvested ( address _startupOwner) external onlyInvestors payable {
+      
     (bool callSuccess,) = payable(_startupOwner).call{value:msg.value}("");
     require(callSuccess,"Transfer Failed");
     startupList[atIndex[_startupOwner]].amount -= msg.value ;
-   }
+    startupList[atIndex[_startupOwner]].upVoteCount ++  ;
+    totalInvested[msg.sender] = msg.value;
+    pushIDX = investedToStartup[msg.sender];
+    uint256 _id = startupList[atIndex[_startupOwner]].sID;
+    pushIDX.push(_id);
+    investedToStartup[msg.sender] = pushIDX ;
+    uint256[] memory _initial;
+    pushIDX = _initial;
 
-   function getCurrentInvestemntNeeded()external onlyStartupOwner view returns(uint256){
-      return startupList[atIndex[msg.sender]].amount ; 
-   }
+    }
+
 
    /*
    *@dev Getter functions
@@ -89,8 +100,19 @@ contract Core {
         return atIndex[msg.sender] ;
     }
 
+   function getInvestedStartupsIdx() internal view returns (uint256[] memory){
+    return investedToStartup[msg.sender] ; 
+   }
+
+   function getCurrentInvestemntNeeded()external  view returns(uint256){
+      return startupList[atIndex[msg.sender]].amount ; 
+   }
    
-    modifier onlyStartupOwner{
+    function getVideoHash() external view returns(string memory){
+        return attachVideoHash[msg.sender];
+    }
+   
+    modifier onlyStartup{
     if (!(sbtFactory.isStartup(msg.sender) ) ){
         revert YOU_HAVENT_BOUGHT_TOKEN();
     }  
@@ -104,11 +126,5 @@ contract Core {
     _;   
     }
 
-   function add (uint a , uint b) external onlyStartupOwner view returns (uint)  {
-    
-   
-    return a + b;
-
-
-   }
+  
 }
--- contract/contracts/Core/StartupDetails.sol
+++ contract/contracts/Core/StartupDetails.sol
@@ -4,9 +4,12 @@ pragma solidity ^0.8.9;
 struct S_Details {
   uint256 sID; 
   string name ;
+  string tagline;
   string description;
   uint256 amount;
-  address ownerAddress;
+  address payable ownerAddress;
   uint256 upVoteCount;
   bool isActive;
+
 }
+
